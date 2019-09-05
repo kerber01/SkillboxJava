f321e5aa9f59b774dd7a87a88fdd0680584146fe
kerber_ <kerber715@gmail.com>
2019-09-05 15:10:54 +0300

    4.2 complete

---
+++ module4/2/MaxVal/src/Loader.java
@@ -0,0 +1,18 @@
+public class Loader {
+
+  public static void main(String[] args) {
+    System.out.println("Double min value is: " + Double.MIN_VALUE);
+    System.out.println("Double max value is: " + Double.MAX_VALUE + "\n");
+    System.out.println("Float min value is: " + Float.MIN_VALUE);
+    System.out.println("Float max value is: " + Float.MAX_VALUE + "\n");
+    System.out.println("Byte min value is: " + Byte.MIN_VALUE);
+    System.out.println("Byte max value is: " + Byte.MAX_VALUE + "\n");
+    System.out.println("Short min value is: " + Short.MIN_VALUE);
+    System.out.println("Short max value is: " + Short.MAX_VALUE + "\n");
+    System.out.println("Integer min value is: " + Integer.MIN_VALUE);
+    System.out.println("Integer max value is: " + Integer.MAX_VALUE + "\n");
+    System.out.println("Long min value is: " + Long.MIN_VALUE);
+    System.out.println("Long max value is: " + Long.MAX_VALUE);
+  }
+
+}
