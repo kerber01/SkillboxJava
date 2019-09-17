a0a844952b74c12c0863880efe93e9f99c94746b
kerber_ <kerber715@gmail.com>
2019-09-17 15:09:27 +0300

    removed 2_hour constant from 7.2

--- module7/2/Airport/src/Loader.java
+++ module7/2/Airport/src/Loader.java
@@ -1,15 +1,18 @@
+import static java.time.temporal.ChronoUnit.HOURS;
+
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
 import com.skillbox.airport.Flight.Type;
 import com.skillbox.airport.Terminal;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Date;
 
 public class Loader {
 
-  private static final long TWO_HOUR_TIME_GAP = 7200000;
+  //private static final long TWO_HOUR_TIME_GAP = 7200000;
 
   public static void main(String[] args) {
 
@@ -29,7 +32,7 @@ public class Loader {
         .map(Terminal::getFlights)
         .flatMap(Collection::stream).sorted(Comparator.comparing(Flight::getDate))
         .filter(f -> f.getType().equals(Type.DEPARTURE)
-            && f.getDate().getTime() - new Date().getTime() <= TWO_HOUR_TIME_GAP
+            && f.getDate().getTime() <= Instant.now().plus(2, HOURS).toEpochMilli()
             && f.getDate().compareTo(new Date()) > 0)
         .forEach(f -> System.out.println(new SimpleDateFormat()
             .format(f.getDate()) + " " + f.getAircraft()
