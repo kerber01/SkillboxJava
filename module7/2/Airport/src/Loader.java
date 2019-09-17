333391bfc17e9381d8b1dc35130d6b9692401748
kerber_ <kerber715@gmail.com>
2019-09-17 14:26:44 +0300

    added map() for 7.2

--- module7/2/Airport/src/Loader.java
+++ module7/2/Airport/src/Loader.java
@@ -1,7 +1,9 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
 import com.skillbox.airport.Flight.Type;
+import com.skillbox.airport.Terminal;
 import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.Date;
 
@@ -11,13 +13,26 @@ public class Loader {
 
   public static void main(String[] args) {
 
-    Airport.getInstance().getTerminals().stream()
+/* Старая версия без map() и до того, как я узнал о flatMap(Collection::stream) для избавления от вложенности
+
+   Airport.getInstance().getTerminals()
         .forEach(t -> t.getFlights().stream().sorted(Comparator.comparing(Flight::getDate))
             .filter(f -> f.getType().equals(Type.DEPARTURE)
-                && f.getDate().getTime() - new Date().getTime() <= TWO_HOUR_TIME_GAP && f.getDate().compareTo(new Date()) > 0)
-            .forEach(f -> System.out.println(new SimpleDateFormat().format(f.getDate()) + " " + f.getAircraft()
-            .getModel())));
+                && f.getDate().getTime() - new Date().getTime() <= TWO_HOUR_TIME_GAP
+                && f.getDate().compareTo(new Date()) > 0)
+            .forEach(f -> System.out
+                .println(new SimpleDateFormat().format(f.getDate()) + " " + f.getAircraft()
+                    .getModel())));
+*/
 
-
+    Airport.getInstance().getTerminals().stream()
+        .map(Terminal::getFlights)
+        .flatMap(Collection::stream).sorted(Comparator.comparing(Flight::getDate))
+        .filter(f -> f.getType().equals(Type.DEPARTURE)
+            && f.getDate().getTime() - new Date().getTime() <= TWO_HOUR_TIME_GAP
+            && f.getDate().compareTo(new Date()) > 0)
+        .forEach(f -> System.out.println(new SimpleDateFormat()
+            .format(f.getDate()) + " " + f.getAircraft()
+            .getModel()));
   }
 }
