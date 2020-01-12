c46580faf76092c9d713bb4738c36a00f1bb9c58
kerber_ <kerber715@gmail.com>
2020-01-12 15:18:37 +0300

    12.1 done

---
+++ module12/1/TodoList/src/main/java/main/DefaultController.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+
+    @RequestMapping("/")
+    public int index() {
+        return Runtime.getRuntime().availableProcessors();
+    }
+}
